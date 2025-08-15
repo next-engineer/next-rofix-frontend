558a6b0ca7776347de7d5734831ef2575f090430
aram <6aram@naver.com>
2025-08-15 23:43:14 +0900

    [fix] workflow 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // Lambda에 필요한 최소 파일만 빌드
-  distDir: ".next", // 기본 디렉토리
+  output: "export",
   images: {
-    unoptimized: true, // Lambda에서 이미지 최적화 제거
+    unoptimized: true,
   },
 };
 
