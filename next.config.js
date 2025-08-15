173defc9e024327ae16ca060426ea761d5609f48
aram <6aram@naver.com>
2025-08-15 22:35:05 +0900

    [fix] ssr lambda, 정적파일 분리

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "standalone", // Lambda에 필요한 최소 파일만 빌드
+  distDir: ".next", // 기본 디렉토리
   images: {
-    unoptimized: true,
+    unoptimized: true, // Lambda에서 이미지 최적화 제거
   },
 };
 
