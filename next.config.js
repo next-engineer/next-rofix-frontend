0b38199f1b308b1d1a6c16ddd20b103148067cdf
aram <6aram@naver.com>
2025-08-16 13:43:49 +0900

    [fix] amplify 배포 위한 작업 - package-lock.json 삭제, pnpm-lock.yml 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // 권장 옵션
-  swcMinify: true, // 빌드 최적화
+  reactStrictMode: true,
   images: {
-    unoptimized: false, // Amplify SSR에서 자동 최적화 사용
+    unoptimized: true, // Amplify에서도 안전
   },
 };
 
